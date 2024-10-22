ecf91fdc1c3b262bfa17b9491cccbe19991bec33
RAJESH GOLE <rajeshgole96@gmail.com>
2024-10-22 23:49:11 +0530

    User Model create

--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,27 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  contactNumber: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  termsAccepted: {
+    type: Boolean,
+    default: false
+  }
 });
 
 // Password hashing middleware
